c14cb1075309624aadef1f7cd883998e2c6a8c00
Anshika Agarwal <AN20090246@wipro.com>
2020-08-26 09:09:47 +0000

    Initial Commit

---
+++ USbank/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using USbank.Models;
+
+namespace USbank.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult AddCTR(string txtCTRNbr, string txtComfirmCTRNbr, string txtTranDate, string txtBusinessline)
+        {
+            
+            return Json("Got this"+txtCTRNbr);
+        }
+        [HttpPost]
+        public JsonResult AddIssue(string txtCTRLogNbr, string txtCustomerType, string txtIssue, string txtIssueType)
+        {
+
+            return Json("Got this" + txtCTRLogNbr);
+        }
+        
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ USbank/wwwroot/js/site.js
@@ -0,0 +1,47 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function myFunction() {
+    var checkBox = document.getElementById("myCheck");
+    var text = document.getElementById("text");
+    if (checkBox.checked == true) {
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+}
+$(function () {
+        var b1 = $("#button1");
+        var b3 = $("#button3");
+        var w = $("#wrapper");
+        var l = $("#list");
+
+        b1.click(function () {
+
+            if (w.hasClass('open')) {
+        w.removeClass('open');
+                w.height(0);
+            } else {
+        w.addClass('open');
+                w.height(l.outerHeight(true));
+            }
+
+        });
+        b3.click(function () {
+
+            if (w.hasClass('open')) {
+                w.removeClass('open');
+                w.height(0);
+            } else {
+                w.addClass('open');
+                w.height(l.outerHeight(true));
+            }
+
+        });
+});
+function Function(x) {
+    x.classList.toggle("fa-minus-circle");
+}
+
